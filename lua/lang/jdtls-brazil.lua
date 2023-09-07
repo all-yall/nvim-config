7b47936fed92091d6ebc1e7ff19b7451e662b8f7
Ally Robinson <iamally@amazon.com>
2023-09-07 13:27:07 -0400

    fix indentation

--- lua/lang/jdtls-brazil.lua
+++ lua/lang/jdtls-brazil.lua
@@ -15,33 +15,33 @@ JDTLS_SETUP = function()
     end
   end
 
-local lombok_jar = home .. "/.config/nvim/java_libs/lombok.jar"
+  local lombok_jar = home .. "/.config/nvim/java_libs/lombok.jar"
   local config = {
     on_attach = require('aerial').on_attach,
     -- The command that starts the language server
-    -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-    cmd = {'jdtls',
-    "--jvm-arg=-javaagent:" .. lombok_jar, -- need for lombok magic
-    "--jvm-arg=-Xbootclasspath/a:".. lombok_jar,
-    "-data", eclipse_workspace,
-    },
+      -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+      cmd = {'jdtls',
+        "--jvm-arg=-javaagent:" .. lombok_jar, -- need for lombok magic
+          "--jvm-arg=-Xbootclasspath/a:".. lombok_jar,
+        "-data", eclipse_workspace,
+      },
     root_dir = root_dir,
     init_options = {
       workspaceFolders = ws_folders_jdtls,
       -- Language server `initializationOptions`
-      -- You need to extend the `bundles` with paths to jar files
-      -- if you want to use additional eclipse.jdt.ls plugins.
-      --
-      -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
-      --
-      -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
-      bundles = {},
+        -- You need to extend the `bundles` with paths to jar files
+        -- if you want to use additional eclipse.jdt.ls plugins.
+        --
+        -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+        --
+        -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+        bundles = {},
     },
 
     -- Here you can configure eclipse.jdt.ls specific settings
-    -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-    -- for a list of options
-    settings = { java = { } },
+      -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+      -- for a list of options
+      settings = { java = { } },
 
     capabilities = owo.cmp.capabilities,
   }
