e19973f4a6f9f638f254da075aba454ebaa4f6dc
Ally Robinson <iamally@amazon.com>
2024-01-30 12:18:12 -0500

    cleanup package list

--- lua/core/packages.lua
+++ lua/core/packages.lua
@@ -1,7 +1,6 @@
 local paq = require 'paq'
 owo.paq = paq
 
--- parinfer
 paq {
   -- Allow paq to manage itself
   'savq/paq-nvim';
@@ -31,16 +30,19 @@ paq {
              cmake --install build --prefix build
              ]] end
            };
-  'simrat39/symbols-outline.nvim';
-  'nvim-neo-tree/neo-tree.nvim';
   'akinsho/bufferline.nvim';
   'lewis6991/gitsigns.nvim';
-  'max397574/better-escape.nvim';
   'goolord/alpha-nvim';
   'rcarriga/nvim-notify';
-  'gioele/vim-autoswap';
+  'kyazdani42/nvim-web-devicons';
 
-  ---- New Commands
+  -- Functional
+  'simrat39/symbols-outline.nvim';
+  'nvim-neo-tree/neo-tree.nvim';
+  'gioele/vim-autoswap';
+  'max397574/better-escape.nvim';
+
+  -- New Commands
   'tpope/vim-fugitive';
   'vim-test/vim-test';
   'junegunn/vim-easy-align';
@@ -51,22 +53,13 @@ paq {
   'hrsh7th/cmp-cmdline';
   'hrsh7th/nvim-cmp';
   'saadparwaiz1/cmp_luasnip';
-  --'CoderCookE/vim-chatgpt';
 
   -- Meta Stuff
   'lewis6991/impatient.nvim';
   'nvim-lua/plenary.nvim'; -- req: a few things? 'kyazdani42/nvim-web-devicons'; -- req: bufferline
   'MunifTanjim/nui.nvim'; -- req: telescope
-  'kyazdani42/nvim-web-devicons';
 
   -- Language Specific
   'mfussenegger/nvim-jdtls';
   'NvChad/nvim-colorizer.lua';
-
-  --  '/apollo/env/envImprovement/vim/amazon/brazil-config';
-  --{'brazil-config',
-    --url='/apollo/env/NinjaHooks/configuration/vim/amazon/brazil-config'};
-  --{'amazon-gbrowse',
-   -- url='ssh://git.amazon.com:2222/pkg/Vim-code-browse'};
-  -- 'jasdel/vim-smithy'; -- lol smithy
 }
