bbcd36de05a3b016e61f8e206b7d206f1bcac185
Ally Robinson <iamally@amazon.com>
2024-10-01 13:42:16 -0400

    fix dumb visual mode escape on jk

--- lua/setup/better_escape.lua
+++ lua/setup/better_escape.lua
@@ -1,3 +1,34 @@
 
-require('better_escape').setup {}
+require("better_escape").setup {
+  timeout = vim.o.timeoutlen,
+  default_mappings = true,
+  mappings = {
+    i = {
+      j = {
+        -- These can all also be functions
+        k = "<Esc>",
+      },
+    },
+    c = {
+      j = {
+        k = "<Esc>",
+      },
+    },
+    t = {
+      j = {
+        k = "<C-\\><C-n>",
+      },
+    },
+    v = {
+      j = {
+        k = false --disable this shortcut
+      }
+    },
+    s = {
+      j = {
+        k = "<Esc>",
+      },
+    },
+  },
+}
 
